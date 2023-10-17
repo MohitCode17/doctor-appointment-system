40f322b84046ece1e83afa1f1ede66bd2638f895
MohitGupta <krishgupta1630@gmail.com>
2023-10-17 21:39:03 +0530

    create routes in application

--- client/src/routes/Routers.jsx
+++ client/src/routes/Routers.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import Home from "../pages/Home";
+import Contact from "../pages/Contact";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+import Services from "../pages/Services";
+import Doctors from "../pages/Doctors/Doctors";
+import DoctorsDetail from "../pages/Doctors/DoctorsDetail";
+
+import { Routes, Route } from "react-router-dom";
 
 const Routers = () => {
-  return <div>Routers</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/doctors/:id" element={<DoctorsDetail />} />
+    </Routes>
+  );
 };
 
 export default Routers;
