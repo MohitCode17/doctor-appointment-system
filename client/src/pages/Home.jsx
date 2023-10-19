89461103e962c162fad703fdc64cd068c31ff8da
MohitGupta <krishgupta1630@gmail.com>
2023-10-19 12:37:50 +0530

    create how it works ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,12 @@
 import HeroImgOne from "../assets/images/hero-img01.png";
 import HeroImgTwo from "../assets/images/hero-img02.png";
 import HeroImgThree from "../assets/images/hero-img03.png";
+import iconOne from "../assets/images/icon01.png";
+import iconTwo from "../assets/images/icon02.png";
+import iconThree from "../assets/images/icon03.png";
+
+import { Link } from "react-router-dom";
+import { BsArrowRight } from "react-icons/bs";
 
 const Home = () => {
   return (
@@ -13,14 +19,18 @@ const Home = () => {
             <div>
               <div className="lg:w-[570px]">
                 <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">
-                  We help patients live a healthy, longer life.
+                  Your Journey to Wellness Starts Here.
                 </h1>
                 <p className="text_para">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-                  ipsa aut ab maiores nisi sit eum tempore sapiente assumenda
-                  corrupti.
+                  Welcome to{" "}
+                  <span className="font-[800] text-primaryColor">Pharma+</span>,
+                  Begin your path to better health with our user-friendly Doctor
+                  Appointment Booking App. Book Appointment with ease and take
+                  charge of your well-being.
                 </p>
-                <button className="btn">Request an Appointment</button>
+                <button className="btn hover:opacity-90">
+                  Book an Appointment
+                </button>
               </div>
               {/* hero counter */}
               <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
@@ -56,6 +66,87 @@ const Home = () => {
               <div className="mt-[30px]">
                 <img src={HeroImgTwo} alt="" className="w-full mb-[30px]" />
                 <img src={HeroImgThree} alt="" className="w-full" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Features */}
+      <section>
+        <div className="container">
+          <div className="lg:w-[580px] mx-auto">
+            <h2 className="heading text-center">
+              Your Journey to Better Health, Simplified
+            </h2>
+            <p className="text_para text-center">
+              Discover how our Doctor Appointment Booking application
+              streamlines the process of finding the right doctor, convenient
+              locations, and booking appointments, all in one place.
+            </p>
+          </div>
+          {/* cards */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt[55px]">
+            <div className="py-[30px] px-5 bg-slate-100 rounded-lg">
+              <div className="flex items-center justify-center">
+                <img src={iconOne} alt="" />
+              </div>
+              <div className="mt-[20px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                  Find the Perfect Doctor
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-400 mt-4 text-center">
+                  With our app, searching for the ideal healthcare provided in a
+                  breeze. Filter by specialty, location and availability.
+                </p>
+                <Link
+                  to={"/doctors"}
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#101A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+            <div className="py-[30px] px-5 bg-slate-100 rounded-lg">
+              <div className="flex items-center justify-center">
+                <img src={iconTwo} alt="" />
+              </div>
+              <div className="mt-[20px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                  Discover Convenient Locations
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-400 mt-4 text-center">
+                  Explore healthcare facilities and clinics near you. Easily
+                  identify and ensuring quality medical care is always within
+                  reach.
+                </p>
+                <Link
+                  to={"/doctors"}
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#101A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+            <div className="py-[30px] px-5 bg-slate-100 rounded-lg">
+              <div className="flex items-center justify-center">
+                <img src={iconThree} alt="" />
+              </div>
+              <div className="mt-[20px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                  Effortless Appointment Booking
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-400 mt-4 text-center">
+                  Seheduling your next doctor's appointment has been easier now.
+                  Choose the time that suits you best, and confirm your visit
+                  with just a few clicks.
+                </p>
+                <Link
+                  to={"/doctors"}
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#101A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
               </div>
             </div>
           </div>
